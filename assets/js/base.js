5973784f23e13b69c25a0b28c385c3b568db04f7
cdlewis <cdlewis@users.noreply.github.com>
2015-08-26 17:19:29 -0400

    add xckd comic to console

---
+++ assets/js/base.js
@@ -0,0 +1,8 @@
+
+jQuery( document ).ready( function( $ ) {
+    $( 'pre' ).addClass( 'prettyprint' );
+    prettyPrint();
+
+    // print xckd 148
+    console.log( '------------------------------------------------\n|                  My Hobby:                   |\n|             Mispronouncing Words             |\n|                                              |\n|  Yeah, did you see what he                   |\n|  said on his wobsite?                        |\n|      \\                         ...His what?  |\n|    Wobsite.                         /        |\n|        \\                     ...I think you  |\n|      Why don\'t you write     mean "website"  |\n|      about it in                /            |\n|      your blag?                /             |\n|           \\                   /              |\n|            O                 O               |\n|           \\|/               \\|/              |\n|            /\\                /\\              |\n|----------------------------------------------|\n' );
+} );
