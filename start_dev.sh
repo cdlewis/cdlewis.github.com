7661c11bbe866c987c9bd4b5992c102cee60731b
Chris Lewis <cdlewis@users.noreply.github.com>
2018-04-08 16:31:51 -0700

    Continue the CSS code golf
    - Move from ul/li menu system to div (requires fewer rules)
    - Add mobile optimised koala picture (improves Google audit score)
    - Continue speculatively deleting and consolidating rules

--- start_dev.sh
+++ start_dev.sh
@@ -4,4 +4,4 @@ ps ax | grep sass | awk '{print $1}' | xargs kill
 
 # Start current dev session
 trap 'kill %1' SIGINT
-bundle exec jekyll serve & node_modules/.bin/node-sass --watch --output-style compressed --sourcemap=none assets/css/style.scss _includes/style.min.css
+bundle exec jekyll serve --incremental & node_modules/.bin/node-sass --watch --output-style compressed assets/css/style.scss _includes/style.min.css
