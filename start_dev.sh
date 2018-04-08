50b4ff978a5d887cb58f54df214c647c1178231b
Chris Lewis <cdlewis@users.noreply.github.com>
2018-04-08 00:59:25 -0700

    Migrate to NPM and upgrade SASS

--- start_dev.sh
+++ start_dev.sh
@@ -1,3 +1,7 @@
-./stop_dev.sh
-bundle exec jekyll serve &
-sass --watch --style compressed --sourcemap=none assets/css/style.scss:_includes/style.min.css &
+# Remove any existing processes
+lsof -n -i4TCP:4000 | grep LISTEN | awk '{print $2}' | xargs kill
+ps ax | grep sass | awk '{print $1}' | xargs kill
+
+# Start current dev session
+trap 'kill %1' SIGINT
+bundle exec jekyll serve & node_modules/.bin/node-sass --watch --output-style compressed --sourcemap=none assets/css/style.scss _includes/style.min.css
--- stop_dev.sh
+++
@@ -1,2 +0,0 @@
-lsof -n -i4TCP:4000 | grep LISTEN | awk '{print $2}' | xargs kill
-ps ax | grep sass | awk '{print $1}' | xargs kill
